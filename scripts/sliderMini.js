32316a1e63210585cadb5cadaaf0a0e05f919212
Kal1Vane <82412065+Kal1Vane@users.noreply.github.com>
2022-01-06 20:36:08 +0500

    Update sliderMini.js

--- scripts/sliderMini.js
+++ scripts/sliderMini.js
@@ -5,7 +5,7 @@ const getRandomInteger = (a = 0, b = 1) => {
 };
 function sliderMini() {
 let number = getRandomInteger(2,8);
-document.body.style.backgroundImage = `url(/img/winter${number}.jpg)`;
+document.body.style.backgroundImage = `url(img/winter${number}.jpg)`;
 }
 
 setInterval(sliderMini,6000);
